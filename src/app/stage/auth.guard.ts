fc6a2482cc4451f10e88d275f51bcc0dd8c9bed3
Kasun Piyumal <kpiyumal90@gmail.com>
2018-07-19 00:23:07 +0530

    guard added

--- src/app/stage/auth.guard.ts
+++ src/app/stage/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivateChild } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MessageService } from '../message.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
   constructor(private messageService: MessageService, private router: Router) {}
   canActivate(
@@ -18,5 +18,9 @@ export class AuthGuard implements CanActivate {
 
       this.router.navigate(['/login']);
     return false;
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    return this.canActivate(childRoute, state);
   }
 }
