39e011960c39ebb6cb92c3b00d4cd12aae95107a
Kasun Piyumal <kpiyumal90@gmail.com>
2018-09-05 18:38:52 +0530

    edit article requests

--- src/app/stage/articles/edit-article/edit-article.component.ts
+++ src/app/stage/articles/edit-article/edit-article.component.ts
@@ -1,6 +1,8 @@
+import { MessageService } from './../../../message.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { Message } from '../../../message';
 
 @Component({
   selector: 'app-edit-article',
@@ -10,15 +12,48 @@ import { Location } from '@angular/common';
 export class EditArticleComponent implements OnInit {
 
   articleKey: string;
+  rawArticle: string;
+  updating: boolean;
+  message: string;
 
-  constructor(private route: ActivatedRoute, private location: Location) { }
+  constructor(private route: ActivatedRoute, private location: Location,
+    private messageService: MessageService) { }
 
   ngOnInit() {
     this.articleKey =  this.route.snapshot.paramMap.get('articleKey');
+    this.requestRawArticle(this.articleKey);
+    this.message = 'No Messages';
+  }
+
+  private requestRawArticle(key: string) {
+    const rawReq = new Message('com.blink.shared.admin.article.RawArticleRequestMessage');
+    rawReq.set('key', key);
+
+    this.messageService.send(rawReq).subscribe(result => {
+      if (result.isOK()) {
+        if (result.getType() !== 'com.blink.shared.system.InvalidRequest') {
+          this.rawArticle = result.get('content');
+        }
+      }
+    });
   }
 
   cancel() {
     this.location.back();
   }
 
+  update() {
+    this.updating = true;
+    const updateReq = new Message('com.blink.shared.admin.article.UpdateArticleRequestMessage');
+    updateReq.set('key', this.articleKey);
+    updateReq.set('content', this.rawArticle);
+
+    this.messageService.send(updateReq).subscribe(result => {
+      this.updating = false;
+      if (result.isOK()) {
+
+      } else {
+      }
+    });
+  }
 }
